e8f7493de5956d0d3c1bfcced11e2d1e8f328c56
Yudi Yamada <yudiyamada10@gmail.com>
2025-03-04 15:59:54 -0300

    feat: setup ThemeProvider and GlobalStyle in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { theme } from "./styles/theme";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/GlobalStyle";
 function App() {
-
   return (
-    <>
-
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <p>Olá, mundo!</p>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
